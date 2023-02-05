2f55cbcef6a1c88c0f8028542656b2770afa51f3
Convitlacbay <convitlacbay@gmail.com>
2023-02-05 23:40:04 +0700

    call apis

---
+++ src/controllers/userController.js
@@ -0,0 +1,34 @@
+import userServices from '../services/userServices'
+
+let handleLogin = async (req, res) => {
+    let email = req.body.email
+    let password = req.body.password
+    if (!email || !password) {
+        return res.status(500).json({
+            errCode: 1,
+            Message: `Missing parameters`
+        })
+    }
+
+    let userData = await userServices.handleUserLogin(email, password)
+
+    return res.status(200).json({
+        // errCode: 0,
+        // Message: 'helo handleLogin',
+        // yourEmail: email,
+        // test: 'your'
+        errCode: userData.errCode,
+        Message: userData.errMessage,
+        userData
+    })
+}
+
+// check email exist
+// compare password
+// return user info
+// access token
+// if (email === '' || email === null || email === 'undefined') 
+
+module.exports = {
+    handleLogin: handleLogin
+}
--- src/route/web.js
+++ src/route/web.js
@@ -1,5 +1,6 @@
 import express from "express";
 import homeController from "../controllers/homeController";
+import userController from "../controllers/userController";
 
 
 let router = express.Router()
@@ -13,6 +14,8 @@ let initWebRoutes = (app) => {
     router.get('/update-crud', homeController.updateCrud)
     router.post('/put-crud', homeController.putCrud)
     router.get('/delete-crud', homeController.deleteCrud)
+
+    router.post('/api/login', userController.handleLogin)
 
 
     return app.use("/", router)
