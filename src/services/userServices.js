da53e06327e97719dfc1cb3246b1d0b2c188cf07
Convitlacbay <convitlacbay@gmail.com>
2023-02-11 17:26:26 +0700

    react-Create-new-user

--- src/services/userServices.js
+++ src/services/userServices.js
@@ -118,24 +118,25 @@ let createNewUsers = (data) => {
             if (check === true) {
                 resolve({
                     errCode: 1,
-                    Message: `your email is already used`
+                    errMessage: `your email is already used`
+                })
+            } else {
+                let hashUserPasswordFromBcrypt = await hashUserPassword(data.password)
+                await db.User.create({
+                    email: data.email,
+                    password: hashUserPasswordFromBcrypt,
+                    firstName: data.firstName,
+                    lastName: data.lastName,
+                    address: data.address,
+                    phoneNumber: data.phoneNumber,
+                    gender: data.gender === '1' ? true : false,
+                    roleId: data.roleId,
+                })
+                resolve({
+                    errCode: 0,
+                    Message: `OK`
                 })
             }
-            let hashUserPasswordFromBcrypt = await hashUserPassword(data.password)
-            await db.User.create({
-                email: data.email,
-                password: hashUserPasswordFromBcrypt,
-                firstName: data.firstName,
-                lastName: data.lastName,
-                address: data.address,
-                phoneNumber: data.phoneNumber,
-                gender: data.gender === '1' ? true : false,
-                roleId: data.roleId,
-            })
-            resolve({
-                errCode: 0,
-                Message: `OK`
-            })
         } catch (err) {
             reject(err);
         }
